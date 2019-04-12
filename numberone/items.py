6ed070c20cdbd1861ca13dc387a694bee9850745
Alex <o.lazoriak-stud343s@chnu.edu.ua>
2019-04-12 20:28:22 +0300

    added some new items

--- numberone/items.py
+++ numberone/items.py
@@ -9,14 +9,27 @@ import scrapy
 
 
 class EkItem(scrapy.Item):
-    #url = scrapy.Field()
     Title = scrapy.Field()
     MemorySize=scrapy.Field()
-    #NameShop= scrapy.Field()
-    #ShopUrl=scrapy.Field()
-    #DiapasonofPrice=scrapy.Field()
     LowPrice=scrapy.Field()
     HighPrice=scrapy.Field()
     CurrencyofPrice=scrapy.Field()
-    #PriceofPhoneInShop=scrapy.Field()
+    ShopsName1= scrapy.Field()
+    ShopsName2= scrapy.Field()
+    ShopsName3= scrapy.Field()
+    ShopsName4= scrapy.Field()
+    ShopsName5= scrapy.Field()
+    ShopsName1Price=scrapy.Field()
+    ShopsName2Price=scrapy.Field()
+    ShopsName3Price=scrapy.Field()
+    ShopsName4Price=scrapy.Field()
+    ShopsName5Price=scrapy.Field()
+    ShopsName1Discription=scrapy.Field()
+    ShopsName2Discription=scrapy.Field()
+    ShopsName3Discription=scrapy.Field()
+    ShopsName4Discription=scrapy.Field()
+    ShopsName5Discription=scrapy.Field()
+
+
+    # ShopUrl=scrapy.Field()
     pass
