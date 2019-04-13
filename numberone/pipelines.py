1aae245b15c57be22d4cc7f3f14f3edcc2689f57
Alex <o.lazoriak-stud343s@chnu.edu.ua>
2019-04-13 21:01:24 +0300

    new class for creating json file and write data in it

--- numberone/pipelines.py
+++ numberone/pipelines.py
@@ -5,7 +5,20 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
+import json
 
 class NumberonePipeline(object):
     def process_item(self, item, spider):
+        return item
+
+class JsonPipeline(object):
+    def open_spider(self,spider):
+        self.file=open('data.jl','w')
+
+    def close_spider(self, spider):
+        self.file.close()
+
+    def process_item(self,item,spider):
+        line=json.dumps(dict(item)) +"\n"
+        self.file.write(line)
         return item
